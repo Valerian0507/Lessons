2ee063176353b4b31e5500ccb16f59bbfb97e160
Alex <example@gmail.com>
2024-02-25 18:06:10 +0100

    Lessons

---
+++ Ознакомление.js
@@ -0,0 +1,25 @@
+let number = 4.6;
+
+console.log(-4/0);
+console.log('sstring' * 9);
+
+const persone = 'Alex';
+
+//const bool = true; или false;
+
+//console.log(something);
+
+let und;
+console.log(und); 
+
+const obj = {
+    name: "John",
+    age: 25,
+    isMarried: false
+};
+
+//console.log(obj.name);
+console.log(obj  ["name"]);
+
+let arr = ['plum.png' , 'orange.jpg' , 6, 'apple.bmp', {},  []];
+console.log(arr[1]);
---
+++ Ознакомление2.js
@@ -0,0 +1,119 @@
+// Интерполяция
+
+"use strict"
+// 
+// const category = 'toys';
+// 
+// console.log(`https://someurl.com/${category}/5`);
+// 
+// const user = "Ivan";
+// 
+// alert(`Привет, ${user}`);
+
+
+
+
+
+
+
+
+// Операторы в JS
+
+// "use strict";
+// 
+// console.log('arr' + "- object");
+// console.log(4 + + "5");
+// 
+// let incr = 10, 
+    // decr = 10; 
+// 
+// ++incr;
+// --decr;
+// 
+// console.log(++incr);
+// console.log(--decr);
+// 
+// console.log(5%2);
+// 
+// console.log(2 + 2 * 2 !== '6');
+// 
+// const isChecked = false,
+// isClose = false;
+// 
+// console.log(!isChecked || isClose);
+
+
+
+
+
+
+// Работа с системой контроля версий Git и GitHub
+
+// Условия
+// if (1) {
+//     console.log('Ok!');
+// } else {
+//     console.log('Error');
+// }
+
+
+
+// if (num < 49) {
+//     console.log('Error');
+// } else if (num > 100) {
+//     consolee.log('Много');
+// } else {
+//     console.log('Ok!');
+// }
+
+// (num === 50) ? console.log('Ok!') : console.logs('Error');
+
+// const num = '50';
+
+// switch (num) {
+//     case '48':
+//         console.log('Неверно');
+//         break;
+//     case '100':
+//         console.log('Неверно');
+//         break;
+//     case '50':
+//         console.log('В точку');
+//         break;
+//     default:
+//         console.log('Не в этот раз');
+//         break;
+// }
+
+
+
+// Циклы
+
+// let num = 50;
+
+// while (num <=55) {
+//     console.log(num);
+//     num++;
+// }
+
+// do {
+//     console.log(num);
+//     num++;
+// }
+// while (num <55);
+
+// for (let i = 1; i < 10; i ++) {
+    // if (i == 6) {
+    // break;
+    // continue;
+    // }
+// 
+//     console.log(i);
+// }
+
+
+
+
+
+
+
