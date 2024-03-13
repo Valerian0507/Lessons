7c069ba5b1727b057b0c5ea8df2e97ab1357bcba
Alex <example@gmail.com>
2024-03-13 22:09:17 +0100

    Add Lessons 21

--- Ознакомление2.js
+++ Ознакомление2.js
@@ -146,7 +146,7 @@
 // function ret() {
 //     let num = 50;
 
-//     // 
+
 
 //     return num;
 // }
@@ -179,30 +179,155 @@
 
 // Методы и свойства строок и числе
 
-const str = "teSt";
-const arr = [1, 2, 4];
+// const str = "teSt";
+// const arr = [1, 2, 4];
 
 // console.log(str[2] = 'd');
 
 // console.log(str.toUpperCase());
-console.log(str.toLowerCase());
-console.log(str);
+// console.log(str.toLowerCase());
+// console.log(str);
 
-const fruit = "Some fruit";
+// const fruit = "Some fruit";
 
-console.log(fruit.indexOf("q"));
+// console.log(fruit.indexOf("q"));
 
-const logg = "Hello World";
+// const logg = "Hello World";
 
 // console.log(logg.slice(6, 11));
 
 // console.log(logg.substring(6, 11));
 
-console.log(logg.substr(6, 5));
+// console.log(logg.substr(6, 5));
 
-const num = 12.2;
-console.log(Math.round(num));
+// const num = 12.2;
+// console.log(Math.round(num));
 
-const text = "12.2px";
+// const text = "12.2px";
 // console.log(parseInt(text));
-console.log(parseFloat(text));
+// console.log(parseFloat(text));
+
+
+
+
+
+
+
+
+
+
+
+// Функции Callback
+
+// function first() {
+    // Do something
+//     setTimeout(function() {
+//         console.log(1);
+//         }, 500);
+// }
+
+// function second() {
+//     console.log(2);
+// }
+
+// first();
+// second();
+
+// function learnJS(lang, callback) {
+//     console.log(`Я учу: ${lang}`);
+//     callback();
+// }
+
+// function done() {
+//     console.log('Я прошел этот урок!');
+// }
+
+// learnJS('JavaScript', done);
+
+
+
+
+// Обьекты, деструктуризация обьектов
+
+
+
+// const options = {
+    // name: 'test',
+    // width: 1024,
+    // height: 1024,
+    // colors: {
+        // border: 'black',
+        // bg: 'red'
+    // },
+    // makeTest: function() {
+        // console.log("Test");
+    // }
+// };
+// 
+// options.makeTest();
+// 
+// const {border, bg} = options.colors;
+// console.log(border);
+
+// Помогает узнать какое количество элементов содержиться в коде
+// console.log(Object.keys(options).length); 
+
+// console.log(options["colors"]["border"]);
+
+// console.log(options.name);
+
+// delete options.name;
+
+// console.log(options);
+
+// let counter = 0;
+// for (let key in options) {
+//     if (typeof(options[key]) === 'object') {
+//         for (let i in options [key]) {
+//             console.log(`Свойство ${i} имееет значение ${options[key][i]}`);
+//         }
+//     } else {
+//         console.log(`Свойство ${key} имееет значение ${options[key]}`);
+//         counter++;
+//     }
+// }
+// console.log(counter);
+
+
+
+
+
+// Массивы и псевдомассивы
+
+// const arr = [2, 23, 16, 38, 210];
+// arr.sort(compareNum);
+// console.log(arr);
+
+// function compareNum(a, b) {
+//     return a - b;
+// }
+
+// arr[99] = 0
+// console.log(arr.length);
+// console.log(arr);
+
+// arr.forEach(function(item, i, arr) {
+    // console.log(`${i}: ${item} внутри массива ${arr}`)
+// });
+
+// arr.pop();
+// arr.push(10);
+// console.log(arr);
+
+// for (let i = 0; i < arr.length; i++) {
+    // console.log(arr[i]);
+// }
+
+// for(let value of arr) {
+    // console.log(value);
+// }
+
+// const str = prompt("", "");
+// const products = str.split(", ");
+// products.sort();
+// console.log(products.join('; '));
