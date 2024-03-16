93d3821bcd4d16959603a7f5229f869d34556b4c
Alex <alex.shaginyan0507@gmail.com>
2024-03-16 16:53:20 +0100

    les 22

--- Ознакомление2.js
+++ Ознакомление2.js
@@ -346,3 +346,42 @@ b = b + 5;
 
 console.log(b);
 console.log(a);
+
+const obj = {
+    a: 5,
+    b: 1
+};
+
+// const copy = obj; // Передает сслыку обьекта выше(передача по ссылке)
+
+// copy.a = 10;
+
+// console.log(copy);
+// console.log(obj);
+
+function copy(mainObj) {
+    let objCopy = {};
+
+    let key;
+    for (key in mainObj) {
+        objCopy[key] = mainObj[key];
+    }
+
+    return objCopy;
+}
+
+const numbers = {
+    a: 2,
+    b: 5,
+    c: {
+        x: 7,
+        y: 4
+    }
+};
+
+const newNumbers = copy(numbers);
+
+newNumbers.a = 10;
+
+console.log(newNumbers);
+console.log(numbers);
