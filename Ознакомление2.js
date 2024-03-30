6dff4e9cd0b37b6c1895cf9424e1a1e5aafdc131
Alex <example@gmail.com>
2024-03-30 17:35:08 +0100

    udemy lessons

--- Ознакомление2.js
+++ Ознакомление2.js
@@ -609,4 +609,46 @@
 
 // 12)
 // 0 || "" || 2 || undefined || true || falsе ? "2";
-console.log(0 || "" || 2 || undefined || true || falsе);
+// console.log(0 || "" || 2 || undefined || true || falsе);
+
+
+
+
+
+
+
+// Замыкание и лексическое окружение
+
+// logNumber();debugger
+
+function createCounter() {
+    let counter = 0;
+
+    const myFunction = function() {debugger
+        counter = counter + 1;debugger
+        return counter;debugger
+    }
+
+    return myFunction;
+}
+debugger
+const increment = createCounter();debugger
+const c1 = increment();debugger
+const c2 = increment();debugger
+const c3 = increment();debugger
+
+console.log(c1,c2, c3);
+
+{
+    let msg = 'Hello';
+}
+
+console.log(msg);
+
+for (let i = 0; i < 9; i ++) {
+    for (let j = 0; j < 9; j ++) {
+        let num = 3;
+    }
+
+    console.log(num);
+}
