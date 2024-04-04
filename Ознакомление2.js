d29433846f01a61221ff73d4931e0fe69e7d3436
Alex <example@gmail.com>
2024-04-04 13:45:51 +0200

    Lesson Udemy

--- Ознакомление2.js
+++ Ознакомление2.js
@@ -797,8 +797,33 @@
 
 
 
+// Test
+// function firstTask() {
+// const arr = [3, 5, 8, 16, 20, 23, 50];
+// const result = [];
+
+// for (let i = 0; i < arr.result; i++) {
+//     result[i] = arr[i];
+// }
+
+// console.log(result);
+// return result;
+// }
 
 
+// Место для второй задачи
+function secondTask() {
+    // Значения массива менять нельзя, тут он проверяется автоматически именно на эти значения
+    const data = [5, 10, 'Shopping', 20, 'Homework'];
 
+for (let i = 0; i < data.lenght; i++) {
+    if(typeof(data[i]) === 'number') {
+        data[i] = data[i] * 2;
+    } else if (typeof(data[i]) === 'string') {
+        data[i] = `${data[i]} - done`;
+    }
+}
 
-
+console.log(data);
+    return data;
+}
